6286fc71cc0c41d4a8e1c6b8bcd5bf145dc70e4d
b19karhj <b19karhj@student.his.se>
2021-04-13 09:46:01 +0200

    Created Second activity and added button to both old and new activity

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -5,22 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.open_second_activity);
-
-
-
+        setContentView(R.layout.activity_second);
 
     }
-
-
-
-
 }
-
-
